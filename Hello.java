305e002b2dbfcb47f8ba3bb50ee7cb533d049a38
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2024-02-11 14:17:33 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class Hello{
+  private void printStmt(){
+    SSLContext context1 = SSLContext.getInstance("SSL");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World"+ context1);
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+  }
+}
